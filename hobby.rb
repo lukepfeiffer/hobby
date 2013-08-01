6230093559796cb0422d09644b18290b7daac4df
Luke Pfeiffer <pthelukenator@gmail.com>
2013-08-01 18:59:21 -0400

    view labels

--- hobby.rb
+++ hobby.rb
@@ -4,7 +4,6 @@ require 'pry'
 require 'json'
 
 get '/' do
-	require 'pry'; binding.pry
 	@hobbies = hobby_list
 	@labels = hobby_list.values.flatten.uniq
   haml :home
@@ -14,7 +13,4 @@ def hobby_list
 	list = YAML.load_file('list.yaml')
 	list_2 = YAML.load_file('list_2.yaml')
 	list.merge(list_2)
-	hobby_list.to_json
 end
-
-hobby_list.to_json
