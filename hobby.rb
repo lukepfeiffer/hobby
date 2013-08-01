0e3894299984933cb34b03dee46e5b098d302d0e
Nathaniel Erskine <racingcamel@gmail.com>
2013-08-01 18:37:29 -0400

    Merge lists

--- hobby.rb
+++ hobby.rb
@@ -4,13 +4,17 @@ require 'pry'
 require 'json'
 
 get '/' do
+	require 'pry'; binding.pry
 	@hobbies = hobby_list
 	@labels = hobby_list.values.flatten.uniq
   haml :home
 end
 
 def hobby_list
-	YAML.load_file('list.yaml')
+	list = YAML.load_file('list.yaml')
+	list_2 = YAML.load_file('list_2.yaml')
+	list.merge(list_2)
+	hobby_list.to_json
 end
 
 hobby_list.to_json
