93edc340082cefa46f317cb6969b6c90aa7197b8
Luke Pfeiffer <pthelukenator@gmail.com>
2013-08-01 18:36:29 -0400

    Convert hash to json

--- hobby.rb
+++ hobby.rb
@@ -1,12 +1,16 @@
 require 'sinatra'
 require 'yaml'
 require 'pry'
+require 'json'
 
 get '/' do
 	@hobbies = hobby_list
+	@labels = hobby_list.values.flatten.uniq
   haml :home
 end
 
 def hobby_list
 	YAML.load_file('list.yaml')
 end
+
+hobby_list.to_json
