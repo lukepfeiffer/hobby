7923fe99e4dc472faff0c3b0b15140f5cf0eec7c
Micah Cooper <mrmicahcooper+mrmicahcooper@gmail.com>
2013-08-22 19:33:22 -0400

    Do that labels

--- hobby.rb
+++ hobby.rb
@@ -4,14 +4,17 @@ require 'json'
 
 class Hobby < Sinatra::Base
   get '/' do
-	  @hobbies = hobby_list
-	  @labels = hobby_list.values.flatten.uniq.sort
+    @labels = hobby_list.values.flatten.uniq.sort
     haml :home
   end
 
+  get '/hobbies' do
+    hobby_list.map{|key,value| {name: key, labels: value} }.to_json
+  end
+
   def hobby_list
-	  list = YAML.load_file('list.yaml')
-	  list_2 = YAML.load_file('list_2.yaml')
-	  list.merge(list_2)
+    list = YAML.load_file('list.yaml')
+    list_2 = YAML.load_file('list_2.yaml')
+    list.merge(list_2)
   end
 end
--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,2 +1,26 @@
 $(function(){
+
+	var hobbies = [];
+
+	$.ajax({
+		url: "/hobbies",
+		dataType: "JSON",
+		success: function(data){
+			hobbies = data
+		}
+	});
+
+	$('div.labels a').click( function(){
+		var button = $(this);
+
+		$.each(hobbies,function(hobby_index, hobby){
+			$.each(hobby['labels'], function(index, label){
+				if(label == button.text()) {
+					console.log(hobby['name'])
+				}
+			});
+		})
+
+	});
+
 })
