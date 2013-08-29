fabf1cdbf8882a0232cce399f4a4e78bb85c6efa
Hashrocket Workstation <dev@hashrocket.com>
2013-08-29 19:36:39 -0400

    Work with labels

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -5,22 +5,29 @@ $(function(){
 	$.ajax({
 		url: "/hobbies",
 		dataType: "JSON",
-		success: function(data){
-			hobbies = data
+		success: function(response){
+			hobbies = response
 		}
 	});
 
-	$('div.labels a').click( function(){
+	$('div.labels a').click(function(){
 		var button = $(this);
+		var number = button.data('index');
 
-		$.each(hobbies,function(hobby_index, hobby){
-			$.each(hobby['labels'], function(index, label){
-				if(label == button.text()) {
-					console.log(hobby['name'])
-				}
-			});
-		})
+		if (button.hasClass("active")){
+			$("."+number+"").remove();
+			button.removeClass("active");
+		}
 
-	});
-
+		else{
+			$.each(hobbies, function(hobby_index, hobby){
+				$.each(hobby['labels'], function(index, label){
+					if(label == button.text()) {
+						$('.suggested_hobbies ul').append("<li class="+number+">"+hobby['name']+"</li>");
+						button.addClass("active");
+					}
+				})
+			})
+		}
+	})
 })
