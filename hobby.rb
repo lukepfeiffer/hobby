324ed7488004f9dfa417a95d6444bcf44f4e3470
Micah Cooper <mrmicahcooper+mrmicahcooper@gmail.com>
2013-09-04 19:01:46 -0400

    Set up rackup

--- hobby.rb
+++ hobby.rb
@@ -1,16 +1,17 @@
 require 'sinatra'
 require 'yaml'
-require 'pry'
 require 'json'
 
-get '/' do
-	@hobbies = hobby_list
-	@labels = hobby_list.values.flatten.uniq.sort
-  haml :home
-end
+class Hobby < Sinatra::Base
+  get '/' do
+	  @hobbies = hobby_list
+	  @labels = hobby_list.values.flatten.uniq.sort
+    haml :home
+  end
 
-def hobby_list
-	list = YAML.load_file('list.yaml')
-	list_2 = YAML.load_file('list_2.yaml')
-	list.merge(list_2)
+  def hobby_list
+	  list = YAML.load_file('list.yaml')
+	  list_2 = YAML.load_file('list_2.yaml')
+	  list.merge(list_2)
+  end
 end
